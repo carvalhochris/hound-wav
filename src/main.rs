303efcf8aa7001995416a133dbee1eb34693538c
Christopher Carvalho <christophercarvalho@Christophers-MacBook-Pro.local>
2024-01-21 19:28:18 +0000

    takes args for freq

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+use hound;
+use std::env;
 use std::f32::consts::PI;
 use std::i16;
-use hound;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let query: &String = &args[1];
+    println!("{}", query);
+    let freq = query.parse::<f32>().unwrap();
     let spec = hound::WavSpec {
         channels: 1,
         sample_rate: 44100,
@@ -10,8 +15,8 @@ fn main() {
         sample_format: hound::SampleFormat::Int,
     };
     let mut writer = hound::WavWriter::create("sine.wav", spec).unwrap();
-    for t in (0 .. 44100).map(|x| x as f32 / 44100.0) {
-        let sample = (t * 440.0 * 2.0 * PI).sin();
+    for t in (0..44100).map(|x| x as f32 / 44100.0) {
+        let sample = (t * freq * 2.0 * PI).sin();
         let amplitude = i16::MAX as f32;
         writer.write_sample((sample * amplitude) as i16).unwrap();
     }
